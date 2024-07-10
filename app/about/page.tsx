bebc8553884de6ccf6a386d7780b5afbdb95bd9e
Harol Calzada <harol.calzada@gmail.com>
2024-07-10 12:03:46 -0500

    fix importation

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,10 @@
 import DownloadAppSection from "./components/Section-7/DownloadAppFinniuSection";
-import TestimonialsDesktop from "../components/Section-6/TestimonialsDesktop";
 import WelcomeSection from "./components/Section-1/WelcomeSection";
 import SponsoredSection from "./components/Section-2/SponsoredSection";
 import PurposeSection from "./components/Section-3/PurposeSection";
 import SolidStrategySection from "./components/Section-4/SolidStrategySection";
 import OurDirectorySection from "./components/Section-5/OurDirectorySection";
+import TestimonialsSection from "../components/Section-6/TestimonialsSection";
 export default function Home() {
   return (
     <main className=" flex min-h-screen flex-col items-center  justify-between ">
@@ -13,7 +13,7 @@ export default function Home() {
       <PurposeSection />
       <SolidStrategySection />
       <OurDirectorySection />
-      <TestimonialsDesktop />
+      <TestimonialsSection />
       <DownloadAppSection />
     </main>
   );
