8026408b6a0dad9045c1bdb6e6105c0adf107946
Ingrid <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2024-08-06 20:38:08 -0500

    download image

--- app/login/components/Login/login.tsx
+++ app/login/components/Login/login.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Image from "next/image";
-import logoTextLight from "@/images/Dashboard/Login/Logo/logo-text-light.png";
+import logoTextLight from "@/images/Dashboard/Login/logoTextLight.png";
 
-const login = () => {
+const Login = () => {
   return (
     <div className="flex h-screen bg-gradient-to-t from-creme-color to-custom-light-blue">
       <div className="m-auto h-[721px] bg-[pink] rounded-[50px] flex flex-col justify-center items-center w-[365px] sm:w-[500px] lg:w-[555px] xl:w-[610px]">
-        {/* <Image
+        <Image
           className="w-[90px] h-[90px] mb-[15px] self-center"
           src={logoTextLight}
           alt="logo"
           height={90}
           width={90}
-        /> */}
+        />
       </div>
     </div>
   );
 };
 
-export default login;
+export default Login;
