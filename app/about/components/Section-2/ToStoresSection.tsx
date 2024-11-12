3848c80d70c22228a4430cc263d360c7086180fa
franco galbiati <franco.galbiati@finniu.com>
2024-11-12 15:30:28 -0300

    fix ( stores ) : fix styles store

--- app/about/components/Section-2/ToStoresSection.tsx
+++ app/about/components/Section-2/ToStoresSection.tsx
@@ -4,19 +4,20 @@ import GooglePlay from "@/images/Section-1/GooglePlay.png";
 import AppStore from "@/images/Section-1/AppStore.png";
 const ToStoresSection = () => {
     return (
-        <section className="flex flex-col justify-center items-center w-full  xl:h-[245px] xl:max-h-[245px] min-h-[245px] bg-toStoresContainer
+        <section className="flex flex-col s:m-5 justify-center items-center w-full  xl:h-[245px] xl:max-h-[245px] min-h-[245px] bg-toStoresContainer
            ">
             <p className="text-black text-[20px] xl:text-[32px] ">¡Descarga Nuestra App!</p>
-            <div className="flex justify-center md:justify-start gap-4 mt-8">
+            <div className="flex justify-center md:justify-center gap-4 mt-8">
                 <a
                     href="https://play.google.com/store/apps/details?id=com.finniu&referrer=utm_source%3Dfinniu_web%26utm_medium%3Dweb%26anid%3Daarki%26aclid%3D{click_id}%26cp1%3D{app_id}"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     <Image
+                        className="w-[150px] lg:w-[240px]"
                         src={GooglePlay}
                         alt="Google-Play"
-                        width={240}
+
                         style={{ height: "auto" }}
                     />
                 </a>
@@ -28,7 +29,7 @@ const ToStoresSection = () => {
                     <Image
                         src={AppStore}
                         alt="App-Store"
-                        width={240}
+                        className="w-[150px] lg:w-[240px]"
                         style={{ height: "auto" }}
                     />
                 </a>
