c3b3b6db557276971eab3e12a23c7832600edebe
Diego De la Vega <diego.delavega@crehana.com>
2024-12-03 16:08:35 -0500

    changes

--- app/components/Section-8/Susbcribe.tsx
+++ app/components/Section-8/Susbcribe.tsx
@@ -1,21 +1,34 @@
+"use client";
+
 import Image from "next/image";
 import Suscribe from "@/images/Section-8/Suscribe.png";
-import FormSusbcribe from "@/components/FormSusbcribe";
-
 
 const Susbcribe = () => {
     return (
-        <section className="w-full h-auto bg-blueDarkColor flex flex-col justify-start items-center p-b-10 sm:p-b-8 md:p-b-10">
-            <Image src={Suscribe} alt="suscribe" className="opacity-80 h-[200px] sm:h-[300px] md:h-[350px]" />
-            <div className="flex flex-col bg-blueDarkColor justify-start items-center w-full max-w-screen-lg py-8 sm:py-10 md:py-12">
-                <h2 className="w-4/5 sm:w-3/4 md:w-[80%] text-center text-white text-[24px] sm:text-[32px] md:text-[38px] font-bold">
-                    Agenda una Video Llamada con Nuestros Asesores Comerciales
-                </h2>
-
-                <FormSusbcribe />
+        <section className="relative w-full h-auto bg-blueDarkColor flex flex-col justify-start items-center p-b-10 sm:p-b-8 md:p-b-10">
+            {/* Imagen con Overlay */}
+            <div className="relative w-full h-[200px] sm:h-[300px] md:h-[350px]">
+                <Image
+                    src={Suscribe}
+                    alt="suscribe"
+                    className="object-cover w-full h-full"
+                />
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center">
+                    <h2 className="text-center text-white text-[24px] sm:text-[32px] md:text-[38px] font-bold">
+                        Agenda una Video Llamada con Nuestros Asesores Comerciales
+                    </h2>
+                    <button
+                        className="mt-6 px-6 py-3 bg-blue-500 text-white text-lg font-semibold rounded-md hover:bg-blue-600 transition duration-300"
+                        onClick={() =>
+                            window.open("https://calendly.com/finniumeet/30min", "_blank")
+                        }
+                    >
+                        Agenda Ahora
+                    </button>
+                </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Susbcribe
+export default Susbcribe;
